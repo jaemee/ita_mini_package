8c6aed243703e313c33aaa8119e57c97f0fc33e3
JAE MELANEE BUGARIN (SHP-IRIS-ISD-OOCLL/MNL) <BUGARJA@OOCL.COM>
2019-10-24 09:50:30 +0800

    initial commit

---
+++ src/main/java/com/tw/itamnl/minipackage/MinipackageApplication.java
@@ -0,0 +1,13 @@
+package com.tw.itamnl.minipackage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinipackageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinipackageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tw/itamnl/minipackage/MinipackageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tw.itamnl.minipackage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinipackageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
