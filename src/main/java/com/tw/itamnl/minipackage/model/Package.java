22b4b0c0297e99ea3951493b14140ab7c9f31b50
JAE MELANEE BUGARIN (SHP-IRIS-ISD-OOCLL/MNL) <BUGARJA@OOCL.COM>
2019-10-24 10:12:50 +0800

    added model for Package and Receiver

---
+++ src/main/java/com/tw/itamnl/minipackage/model/Package.java
@@ -0,0 +1,85 @@
+package com.tw.itamnl.minipackage.model;
+
+import org.hibernate.annotations.GeneratorType;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+public class Package {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid")
+    private Long id;
+
+    @NotNull
+    @Column(unique = true)
+    private Integer packageNo;
+
+    @OneToOne
+    private PackageReceiver receiver;
+
+    private String weight;
+    private String status;
+    private LocalDateTime bookingTime;
+    private LocalDateTime pickUpTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getPackageNo() {
+        return packageNo;
+    }
+
+    public void setPackageNo(Integer packageNo) {
+        this.packageNo = packageNo;
+    }
+
+    public PackageReceiver getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(PackageReceiver receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getBookingTime() {
+        return bookingTime;
+    }
+
+    public void setBookingTime(LocalDateTime bookingTime) {
+        this.bookingTime = bookingTime;
+    }
+
+    public LocalDateTime getPickUpTime() {
+        return pickUpTime;
+    }
+
+    public void setPickUpTime(LocalDateTime pickUpTime) {
+        this.pickUpTime = pickUpTime;
+    }
+}
---
+++ src/main/java/com/tw/itamnl/minipackage/model/PackageReceiver.java
@@ -0,0 +1,43 @@
+package com.tw.itamnl.minipackage.model;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class PackageReceiver {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid")
+    private Long id;
+
+    private String name;
+    private Integer phoneNumber;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
